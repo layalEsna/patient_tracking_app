59d1ea45bfc69db755f05ed61597aab0f6a7a957
Amene <layla_1380@yahoo.com>
2024-10-30 21:01:53 -0500

    Add Patient class with attributes and automatic ID assignment

--- models/patient.py
+++ models/patient.py
@@ -1,1 +1,14 @@
         
+
+
+
+class Patient:
+    all = []
+    def __init__(self, name, lastname, age, disease, doctor_id, id=None):
+        self.id = id or len(Patient.all) +1
+        self.name = name
+        self.lastname = lastname
+        self.age = age
+        self.disease = disease
+        self.doctor_id = doctor_id
+        Patient.all.append(self)

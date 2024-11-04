f066ceb4d449f61e2f1b344d853b24276fdad209
Amene <layla_1380@yahoo.com>
2024-11-04 09:55:47 -0600

    Separate doctor information options into a new doctors_info() function

--- utils/cli.py
+++ utils/cli.py
@@ -4,50 +4,80 @@ from utils.helpers import *
 from seed import seed_database
 import sys
 
-def display_welcome_and_instructions():
-    print("Welcome to the Patient Tracking App\n")
-    print("Please select an option:\n")
-    print("1. Display doctors list\n")
-    print("2. Update a doctor's information\n")
-    print("3. Add a new doctor\n")
-    print("4. Display patients list\n")
-    print("5. Update a patient's information\n")
-    print("6. Add a new patient\n")
-    print("7. Delete a doctor\n")
-    print("8. Delete a patient\n")
-    print("9. Exit\n")
-
-def exit_program():
-    print('Goodbye!')
-    sys.exit()
-
-def main():
-    seed_database()  
+def doctors_info():
     while True:
-        display_welcome_and_instructions()
-        choice = input("Enter your choice: ")
+        print('\nDoctors Information:\n')
+        print('a) Doctors List:\n')
+        print("b) Update a doctor's information\n")
+        print("c) Add a new doctor\n")
+        print("d) Delete a doctor\n")
+        print('m) Main Menu\n')
+        print("0- Exit\n")
 
-        if choice == '1':
+        choice = input('\nEnter your choice: \n').lower()
+        if choice == 'a':
             doctors_list()
-        elif choice == '2':
+        elif choice == 'b':
             update_doctor()
-        elif choice == '3':
+        elif choice == 'c':
             add_doctor()
-        elif choice == '4':
-            patients_list()
-        elif choice == '5':
-            update_patient()
-        elif choice == '6':
-            add_patient()
-        elif choice == '7':
+        elif choice == 'd':
             delete_doctor()
-        elif choice == '8':
-            delete_patient()
-        elif choice == '9':
-            exit_program()
+        elif choice == 'm':
+            main_menu()
+        elif choice == '0':
+            sys.exit('Goodbye!\n')
         else:
-            print('Invalid choice, please try again.')
+            print('Invalid choice. Please try again.\n')
 
-if __name__ == '__main__':
-    main()
+
+
+def patients_info():
+   while True:
+       print("\nPatients information:\n")
+       print("e) Patients List:\n")
+       print("f) Update a patient's information\n")
+       print("g) Add a new patient\n")
+       print("h) Delete a patient\n")
+       print('m) Main Menu\n')
+       print("0- Exit\n")
+
+       choice = input('Enter your choice: \n').lower()
+       if choice == 'e':
+            patients_list()
+       elif choice == 'f':
+            update_patient()
+       elif choice == 'g':
+            add_patient()
+       elif choice == 'h':
+            delete_patient()
+       elif choice == 'm':
+           main_menu()
+       elif choice == '0':
+            sys.exit('Goodbye!')
+       else:
+            print('Invalis choice. Please try again.\n')
+
+
+def main_menu():
+    while True:
+        print('\n*****Main Menu*****\n')
+        print('Select a number:\n')
+        print('1- Doctors Info...\n')
+        print('2-Patients Info...\n')
+        print('0- Exit...\n')
+        choice = input('\nEnter your  choice: \n')
+        if choice == '1':
+            doctors_info()
+        elif choice == '2':
+            patients_info()
+        elif choice == '0':
+            sys.exit('Goodbye\n')
+        else:
+            print('Invalid number. Please try again.\n')
+
+
+if __name__ == "__main__":
+    main_menu()
+    
 # PYTHONPATH=. python -m utils.cli
